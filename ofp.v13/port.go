e21c2064895c3ecdad9fe870b3fef1d8f7c56d35
Yakau Bubnou <ybubnou@infoblox.com>
2016-11-04 20:38:04 +0300

    Port stringers and statistics
    This patch provides implementation of the marshaling/unmarshaling
    methods for port statistics, port statistics requet, port status
    and port modification OpenFlow messages.
    
    Also, it defines the stringer methods for port features, status
    and configuration.

--- ofp.v13/port.go
+++ ofp.v13/port.go
@@ -1,163 +1,133 @@
 package ofp
 
 import (
+	"fmt"
 	"io"
 	"net"
-	"strings"
 
 	"github.com/netrack/openflow/encoding"
 )
 
 const (
-	PF_10MB_HD  PortFeatures = 1 << iota
-	PF_10MB_FD  PortFeatures = 1 << iota
-	PF_100MB_HD PortFeatures = 1 << iota
-	PF_100MB_FD PortFeatures = 1 << iota
-	PF_1GB_HD   PortFeatures = 1 << iota
-	PF_1GB_FD   PortFeatures = 1 << iota
-	PF_10GB_FD  PortFeatures = 1 << iota
-	PF_40GB_FD  PortFeatures = 1 << iota
-	PF_100GB_FD PortFeatures = 1 << iota
-	PF_1TB_FD   PortFeatures = 1 << iota
-	PF_OTHER    PortFeatures = 1 << iota
+	PortFeature10MbitHalfDuplex  PortFeature = 1 << iota
+	PortFeature10MbitFullDuplex  PortFeature = 1 << iota
+	PortFeature100MbitHalfDuplex PortFeature = 1 << iota
+	PortFeature100MbitFullDuplex PortFeature = 1 << iota
+	PortFeature1GbitHalfDuplex   PortFeature = 1 << iota
+	PortFeature1GbitFullDuplex   PortFeature = 1 << iota
+	PortFeature10GbitFullDuplex  PortFeature = 1 << iota
+	PortFeature40GbitFullDuplex  PortFeature = 1 << iota
+	PortFeature100GbitFullDuplex PortFeature = 1 << iota
+	PortFeature1TbitFullDuplex   PortFeature = 1 << iota
+	PortFeatureOther             PortFeature = 1 << iota
 
-	PF_COPPER     PortFeatures = 1 << iota
-	PF_FIBER      PortFeatures = 1 << iota
-	PF_AUTONEG    PortFeatures = 1 << iota
-	PF_PAUSE      PortFeatures = 1 << iota
-	PF_PAUSE_ASYM PortFeatures = 1 << iota
+	PortFeatureCopper    PortFeature = 1 << iota
+	PortFeatureFiber     PortFeature = 1 << iota
+	PortFeatureAutoneg   PortFeature = 1 << iota
+	PortFeaturePause     PortFeature = 1 << iota
+	PortFeaturePauseAsym PortFeature = 1 << iota
 )
 
-type PortFeatures uint32
+const (
+	portFeatureSpeedMask  = 0x000007ff
+	portFeatureMediumMask = 0x0000f800
+)
 
-func (f PortFeatures) String() string {
-	// 10Mbps full-duplex copper
-	var repr string
+type PortFeature uint32
 
-	switch {
-	case f&PF_10MB_HD != 0:
-		repr = "10 Mbps half-duplex"
-	case f&PF_10MB_FD != 0:
-		repr = "10 Mbps full-duplex"
-	case f&PF_100MB_HD != 0:
-		repr = "100 Mbps half-duplex"
-	case f&PF_100MB_FD != 0:
-		repr = "100 Mbps full-duplex"
-	case f&PF_1GB_HD != 0:
-		repr = "1 Gbps half-duplex"
-	case f&PF_1GB_FD != 0:
-		repr = "1 Gbps full-duplex"
-	case f&PF_10GB_FD != 0:
-		repr = "10 Gbps full-duplex"
-	case f&PF_40GB_FD != 0:
-		repr = "40 Gbps full-duplex"
-	case f&PF_100GB_FD != 0:
-		repr = "100 Gbps full-duplex"
-	case f&PF_1TB_FD != 0:
-		repr = "1 Tbps full-duplex"
-	case f&PF_OTHER != 0:
-		repr = "other"
-	default:
-		repr = "unknown"
+var portFeaturesText = map[PortFeature]string{
+	PortFeature10MbitHalfDuplex:  "10 Mbps half-duplex",
+	PortFeature10MbitFullDuplex:  "10 Mbps full-duplex",
+	PortFeature100MbitHalfDuplex: "100 Mbps half-duplex",
+	PortFeature100MbitFullDuplex: "100 Mbps full-duplex",
+	PortFeature1GbitHalfDuplex:   "1 Gbps half-duplex",
+	PortFeature1GbitFullDuplex:   "1 Gbps full-duplex",
+	PortFeature10GbitFullDuplex:  "10 Gbps full-duplex",
+	PortFeature40GbitFullDuplex:  "40 Gbps full-duplex",
+	PortFeature100GbitFullDuplex: "100 Gbps full-duplex",
+	PortFeature1TbitFullDuplex:   "1 Tbps full-duplex",
+	PortFeatureOther:             "other",
+	PortFeatureCopper:            "copper",
+	PortFeatureFiber:             "fiber",
+	PortFeatureAutoneg:           "autoneg",
+	PortFeaturePause:             "pause",
+	PortFeaturePauseAsym:         "pause asym",
+}
+
+func (f PortFeature) String() string {
+	speed, ok := portFeaturesText[f&portFeatureSpeedMask]
+	if !ok {
+		speed = "unknown"
 	}
 
-	switch {
-	case f&PF_COPPER != 0:
-		repr += " copper"
-	case f&PF_FIBER != 0:
-		repr += " fiber"
-	case f&PF_AUTONEG != 0:
-		repr += " autoneg"
-	case f&PF_PAUSE != 0:
-		repr += " pause"
-	case f&PF_PAUSE_ASYM != 0:
-		repr += " pause asym"
-	default:
-		repr += " unknown"
+	medium, ok := portFeaturesText[f&portFeatureMediumMask]
+	if !ok {
+		medium = "unknown"
 	}
 
-	return repr
+	return fmt.Sprintf("%s %s", speed, medium)
 }
 
 const (
 	// Port is administratively down
-	PC_PORT_DOWN PortConfig = 1 << iota
+	PortConfigDown PortConfig = 1 << iota
 
 	// Drop all packets received by port
-	PC_NO_RCV PortConfig = 1 << iota
+	PortConfigNoRcv PortConfig = 1 << iota
 
 	// Drop packets forwarded to port
-	PC_NO_FWD PortConfig = 1 << iota
+	PortConfigNoFwd PortConfig = 1 << iota
 
 	// Do not send packet-in msgs for port
-	PC_NO_PACKET_IN PortConfig = 1 << iota
+	PortConfigNoPacketIn PortConfig = 1 << iota
 )
 
 type PortConfig uint32
 
+var portConfigText = map[PortConfig]string{
+	PortConfigDown:       "down",
+	PortConfigNoRcv:      "no recv",
+	PortConfigNoFwd:      "no fwd",
+	PortConfigNoPacketIn: "no packet in",
+}
+
 func (c PortConfig) String() string {
-	var repr []string
-
-	if c&PC_PORT_DOWN != 0 {
-		repr = append(repr, "DOWN")
+	if text, ok := portConfigText[c]; ok {
+		return text
 	}
 
-	if c&PC_NO_RCV != 0 {
-		repr = append(repr, "NO_RCV")
-	}
-
-	if c&PC_NO_FWD != 0 {
-		repr = append(repr, "NO_FWD")
-	}
-
-	if c&PC_NO_PACKET_IN != 0 {
-		repr = append(repr, "NO_PACKET_IN")
-	}
-
-	if len(repr) == 0 {
-		repr = append(repr, "UP")
-	}
-
-	return strings.Join(repr, ",")
+	return "up"
 }
 
 const (
 	// PS_LINK_DOWN bit indicates that the physical link is not present.
-	PS_LINK_DOWN PortState = 1 << iota
+	PortStateLinkDown PortState = 1 << iota
 
 	// PS_BLOCKED bit indicates that a switch protocol outside
 	// of OpenFlow, such as 802.1D Spanning Tree, is preventing
 	// the use of that port with OFPP_FLOOD.
-	PS_BLOCKED PortState = 1 << iota
+	PortStateBlocked PortState = 1 << iota
 
 	// PS_LIVE indicates that port available for live for Fast Failover Group
-	PS_LIVE PortState = 1 << iota
+	PortStateLive PortState = 1 << iota
 )
 
 // Current state of the physical port. These are
 // not configurable from the controller
 type PortState uint32
 
+var portStateText = map[PortState]string{
+	PortStateLinkDown: "link down",
+	PortStateBlocked:  "blocked",
+	PortStateLive:     "live",
+}
+
 func (s PortState) String() string {
-	var repr []string
-
-	if s&PS_LINK_DOWN != 0 {
-		repr = append(repr, "LINK_DOWN")
+	if text, ok := portStateText[s]; ok {
+		return text
 	}
 
-	if s&PS_BLOCKED != 0 {
-		repr = append(repr, "BLOCKED")
-	}
-
-	if s&PS_LIVE != 0 {
-		repr = append(repr, "LIVE")
-	}
-
-	if len(repr) == 0 {
-		repr = append(repr, "LINK_UP")
-	}
-
-	return strings.Join(repr, ",")
+	return "link up"
 }
 
 const (
@@ -197,7 +167,7 @@ const (
 
 type PortNo uint32
 
-const MAX_PORT_NAME_LEN = 16
+const portNameLen = 16
 
 // The port description request MP_PORT_DESCRIPTION enables the
 // controller to get a description of all the ports in the system
@@ -206,19 +176,19 @@ const MAX_PORT_NAME_LEN = 16
 type Port struct {
 	PortNo PortNo
 	HWAddr net.HardwareAddr
-	Name   []byte
+	Name   string
 
 	Config PortConfig
 	State  PortState
 
 	// Current features
-	Curr PortFeatures
+	Curr PortFeature
 	// Features being advertised by the port
-	Advertised PortFeatures
+	Advertised PortFeature
 	// Features supported by the port
-	Supported PortFeatures
+	Supported PortFeature
 	// Features advertised by peer
-	Peer PortFeatures
+	Peer PortFeature
 
 	// Current port bitrate in kbps
 	CurrSpeed uint32
@@ -226,16 +196,37 @@ type Port struct {
 	MaxSpeed uint32
 }
 
+func (p *Port) WriteTo(w io.Writer) (int64, error) {
+	name := make([]byte, portNameLen)
+	copy(name, []byte(p.Name))
+
+	return encoding.WriteTo(w,
+		p.PortNo,
+		pad4{},
+		p.HWAddr,
+		pad2{},
+		name,
+		p.Config,
+		p.State,
+		p.Curr,
+		p.Advertised,
+		p.Supported,
+		p.Peer,
+		p.CurrSpeed,
+		p.MaxSpeed,
+	)
+}
+
 func (p *Port) ReadFrom(r io.Reader) (int64, error) {
 	p.HWAddr = make(net.HardwareAddr, 6)
-	p.Name = make([]byte, MAX_PORT_NAME_LEN)
+	var name [portNameLen]byte
 
-	return encoding.ReadFrom(r,
+	n, err := encoding.ReadFrom(r,
 		&p.PortNo,
-		&pad4{},
+		&defaultPad4,
 		&p.HWAddr,
-		&pad2{},
-		&p.Name,
+		&defaultPad2,
+		&name,
 		&p.Config,
 		&p.State,
 		&p.Curr,
@@ -245,6 +236,9 @@ func (p *Port) ReadFrom(r io.Reader) (int64, error) {
 		&p.CurrSpeed,
 		&p.MaxSpeed,
 	)
+
+	p.Name = string(name[:])
+	return n, err
 }
 
 type Ports []Port
@@ -270,42 +264,72 @@ func (p *Ports) ReadFrom(r io.Reader) (n int64, err error) {
 
 type PortMod struct {
 	PortNo    PortNo
-	_         pad4
 	HWAddr    net.HardwareAddr
-	_         pad2
 	Config    PortConfig
 	Mask      PortConfig
-	Advertise PortFeatures
-	_         pad4
+	Advertise PortFeature
+}
+
+func (p *PortMod) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w,
+		p.PortNo, pad4{},
+		p.HWAddr, pad2{},
+		p.Config,
+		p.Mask,
+		p.Advertise, pad4{},
+	)
+}
+
+func (p *PortMod) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r,
+		&p.PortNo, &defaultPad4,
+		&p.HWAddr, &defaultPad2,
+		&p.Config,
+		&p.Mask,
+		&p.Advertise, &defaultPad4,
+	)
 }
 
 const (
 	// The port was added
-	PR_ADD PortReason = iota
+	PortReasonAdd PortReason = iota
 
 	// The port was removed
-	PR_DELETE
+	PortReasonDelete
 
 	// Some attribute of the port has changed
-	PR_MODIFY
+	PortReasonModify
 )
 
 type PortReason uint8
 
 type PortStatus struct {
 	Reason PortReason
-	_      pad7
-	Desc   Port
+	Port   Port
+}
+
+func (p *PortStatus) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w, p.Reason, pad7{}, &p.Port)
+}
+
+func (p *PortStatus) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &p.Reason, &defaultPad7, &p.Port)
 }
 
 type PortStatsRequest struct {
 	PortNo PortNo
-	_      pad4
+}
+
+func (p *PortStatsRequest) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w, p.PortNo, pad4{})
+}
+
+func (p *PortStatsRequest) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &p.PortNo, &defaultPad4)
 }
 
 type PortStats struct {
 	PortNo       PortNo
-	_            pad4
 	RxPackets    uint64
 	TxPackets    uint64
 	RxBytes      uint64
@@ -320,4 +344,46 @@ type PortStats struct {
 	Collisions   uint64
 	DurationSec  uint32
 	DurationNSec uint32
+}
+
+func (p *PortStats) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w,
+		p.PortNo,
+		pad4{},
+		p.RxPackets,
+		p.TxPackets,
+		p.RxBytes,
+		p.TxBytes,
+		p.RxDropped,
+		p.TxDropped,
+		p.RxErrors,
+		p.TxErrors,
+		p.RxFrameErr,
+		p.RxOverErr,
+		p.RxCrcErr,
+		p.Collisions,
+		p.DurationSec,
+		p.DurationNSec,
+	)
+}
+
+func (p *PortStats) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r,
+		&p.PortNo,
+		&defaultPad4,
+		&p.RxPackets,
+		&p.TxPackets,
+		&p.RxBytes,
+		&p.TxBytes,
+		&p.RxDropped,
+		&p.TxDropped,
+		&p.RxErrors,
+		&p.TxErrors,
+		&p.RxFrameErr,
+		&p.RxOverErr,
+		&p.RxCrcErr,
+		&p.Collisions,
+		&p.DurationSec,
+		&p.DurationNSec,
+	)
 }
