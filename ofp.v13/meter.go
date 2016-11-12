7e06d6ba918905f133390929271aae43f73ed64b
Yasha Bubnov <ybubnou@infoblox.com>
2016-11-12 21:49:34 +0300

    Meter message marshaling (#34)
    This patch provides implementation of the marshaling and unmarshaling
    for the following messages: meter modification, meter statistics, meter
    features, meter configuration multipart request.

--- ofp.v13/meter.go
+++ ofp.v13/meter.go
@@ -1,78 +1,276 @@
 package ofp
 
-const (
-	MC_ADD MeterModCommands = iota
-	MC_MODIFY
-	MC_DELETE
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/netrack/openflow/encoding"
 )
 
-type MeterModCommands uint16
-
 const (
-	MF_KBPS MeterFlags = iota
-	MF_PKTPS
-	MF_BURST
-	MF_STATS
+	MeterCommandAdd MeterCommand = iota
+	MeterCommandModify
+	MeterCommandDelete
 )
 
-type MeterFlags uint16
+type MeterCommand uint16
 
 const (
-	M_MAX        Meter = 0xffff0000
-	M_SLOWPATH   Meter = 0xfffffffd
-	M_CONTROLLER Meter = 0xfffffffe
-	M_ALL        Meter = 0xffffffff
+	MeterFlagKBitPerSec   MeterFlag = 1 << iota
+	MeterFlagPacketPerSec MeterFlag = 1 << iota
+	MeterFlagBurst        MeterFlag = 1 << iota
+	MeterFlagStats        MeterFlag = 1 << iota
+)
+
+type MeterFlag uint16
+
+const (
+	MeterMax        Meter = 0xffff0000
+	MeterSlowpath   Meter = 0xfffffffd
+	MeterController Meter = 0xfffffffe
+	MeterAll        Meter = 0xffffffff
 )
 
 type Meter uint32
 
-type MeterMod struct {
-	Command MeterModCommands
-	Flags   MeterFlags
-	MeterID Meter
-	Bands   []MeterBandHeader
-}
-
 const (
-	MBT_DROP         MeterBandType = 1
-	MBT_DSCP_REMARK  MeterBandType = 2
-	MBT_EXPERIMENTER MeterBandType = 0xFFFF
+	MeterBandTypeDrop         MeterBandType = 1 + iota
+	MeterBandTypeDSCPRemark   MeterBandType = 1 + iota
+	MeterBandTypeExperimenter MeterBandType = 0xffff
 )
 
 type MeterBandType uint16
 
-type MeterBandHeader struct {
-	Type      MeterBandType
-	Length    uint16
+var meterBandMap = map[MeterBandType]encoding.ReaderMaker{
+	MeterBandTypeDrop:         encoding.ReaderMakerOf(MeterBandDrop{}),
+	MeterBandTypeDSCPRemark:   encoding.ReaderMakerOf(MeterBandDSCPRemark{}),
+	MeterBandTypeExperimenter: encoding.ReaderMakerOf(MeterBandExperimenter{}),
+}
+
+type meterBand struct {
+	Type MeterBandType
+	Len  uint16
+}
+
+const meterBandLen = 16
+
+type MeterBand interface {
+	encoding.ReadWriter
+
+	Type() MeterBandType
+}
+
+type MeterBands []MeterBand
+
+func (m MeterBands) WriteTo(w io.Writer) (int64, error) {
+	var n int64
+
+	for _, meter := range m {
+		nn, err := meter.WriteTo(w)
+		n += nn
+
+		if err != nil {
+			return n, err
+		}
+	}
+
+	return n, nil
+}
+
+func (m MeterBands) ReadFrom(r io.Reader) (int64, error) {
+	var meterBandType MeterBandType
+
+	rm := func() (io.ReaderFrom, error) {
+		if rm, ok := meterBandMap[meterBandType]; ok {
+			rd, err := rm.MakeReader()
+			m = append(m, rd.(MeterBand))
+			return rd, err
+		}
+
+		format := "ofp: unknown meter band type: '%x'"
+		return nil, fmt.Errorf(format, meterBandType)
+	}
+
+	return encoding.ScanFrom(r, &meterBandType,
+		encoding.ReaderMakerFunc(rm))
+}
+
+type MeterBandDrop struct {
 	Rate      uint32
 	BurstSize uint32
 }
 
-type MeterBandDrop struct {
-	MeterBandHeader
-	_ pad4
+func (m *MeterBandDrop) Type() MeterBandType {
+	return MeterBandTypeDrop
 }
 
-type MeterBandDscpRemart struct {
-	MeterBandHeader
+func (m *MeterBandDrop) WriteTo(w io.Writer) (int64, error) {
+	header := meterBand{m.Type(), meterBandLen}
+	return encoding.WriteTo(w, header, m.Rate, m.BurstSize, pad4{})
+}
+
+func (m *MeterBandDrop) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &meterBand{},
+		&m.Rate, &m.BurstSize, &defaultPad4)
+}
+
+type MeterBandDSCPRemark struct {
+	Rate      uint32
+	BurstSize uint32
 	PrecLevel uint8
-	_         pad3
+}
+
+func (m *MeterBandDSCPRemark) Type() MeterBandType {
+	return MeterBandTypeDSCPRemark
+}
+
+func (m *MeterBandDSCPRemark) WriteTo(w io.Writer) (int64, error) {
+	header := meterBand{m.Type(), meterBandLen}
+	return encoding.WriteTo(w, header, m.Rate,
+		m.BurstSize, m.PrecLevel, pad3{})
+}
+
+func (m *MeterBandDSCPRemark) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &meterBand{},
+		&m.Rate, &m.BurstSize, &m.PrecLevel, &defaultPad3)
 }
 
 type MeterBandExperimenter struct {
-	MeterBandHeader
+	Rate         uint32
+	BurstSize    uint32
 	Experimenter uint32
 }
 
-type MeterMultipartRequest struct {
-	MeterID Meter
-	_       pad4
+func (m *MeterBandExperimenter) Type() MeterBandType {
+	return MeterBandTypeExperimenter
 }
 
+func (m *MeterBandExperimenter) WriteTo(w io.Writer) (int64, error) {
+	header := meterBand{m.Type(), meterBandLen}
+	return encoding.WriteTo(w, header, m.Rate,
+		m.BurstSize, m.Experimenter)
+}
+
+func (m *MeterBandExperimenter) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &meterBand{}, &m.Rate,
+		&m.BurstSize, &m.Experimenter)
+}
+
+type MeterMod struct {
+	Command MeterCommand
+	Flags   MeterFlag
+	Meter   Meter
+	Bands   MeterBands
+}
+
+func (m *MeterMod) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w, m.Command, m.Flags, m.Meter, m.Bands)
+}
+
+func (m *MeterMod) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &m.Command, &m.Flags, &m.Meter, m.Bands)
+}
+
+type MeterConfigRequest struct {
+	Meter Meter
+}
+
+func (m *MeterConfigRequest) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w, m.Meter, pad4{})
+}
+
+func (m *MeterConfigRequest) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &m.Meter, &defaultPad4)
+}
+
+const meterConfigLen = 8
+
+type MeterConfig struct {
+	Flags MeterFlag
+	Meter Meter
+	Bands MeterBands
+}
+
+func (m *MeterConfig) WriteTo(w io.Writer) (int64, error) {
+	// Write the list of meter bands to the temporary
+	// buffer to calculate the total length of the message.
+	var buf bytes.Buffer
+	_, err := m.Bands.WriteTo(&buf)
+	if err != nil {
+		return 0, err
+	}
+
+	length := uint16(meterConfigLen + buf.Len())
+	return encoding.WriteTo(w, length, m.Flags, m.Meter, buf.Bytes())
+}
+
+func (m *MeterConfig) ReadFrom(r io.Reader) (int64, error) {
+	var length uint16
+
+	// Read the header of the message, so we could use
+	// the rest of the reader to unmarshal list of bands.
+	n, err := encoding.ReadFrom(r, &length, &m.Flags, &m.Meter)
+	if err != nil {
+		return n, err
+	}
+
+	// Use the rest of bytes to decode the bands.
+	limrd := io.LimitReader(r, int64(length-meterConfigLen))
+	nn, err := m.Bands.ReadFrom(limrd)
+	return n + nn, err
+}
+
+// meterFeaturesBandTypesLen is a length of the list of band
+// types bitmap of meter features.
+const meterFeaturesBandTypesLen = 2
+
+type MeterFeatures struct {
+	MaxMeter     uint32
+	BandTypes    []MeterBandType
+	Capabilities MeterFlag
+	MaxBands     uint8
+	MaxColor     uint8
+}
+
+func (m *MeterFeatures) init() []MeterBandType {
+	return make([]MeterBandType, meterFeaturesBandTypesLen)
+}
+
+func (m *MeterFeatures) WriteTo(w io.Writer) (int64, error) {
+	types := m.init()
+	copy(types, m.BandTypes)
+
+	return encoding.WriteTo(w, m.MaxMeter, types,
+		m.Capabilities, m.MaxBands, m.MaxColor, pad2{})
+}
+
+func (m *MeterFeatures) ReadFrom(r io.Reader) (int64, error) {
+	m.BandTypes = m.init()
+
+	return encoding.ReadFrom(r, &m.MaxMeter, &m.BandTypes,
+		&m.Capabilities, &m.MaxBands, &m.MaxColor, &defaultPad2)
+}
+
+type MeterBandStats struct {
+	PacketBandCount uint64
+	ByteBandCount   uint64
+}
+
+func (m *MeterBandStats) WriteTo(w io.Writer) (int64, error) {
+	return encoding.WriteTo(w, m.PacketBandCount, m.ByteBandCount)
+}
+
+func (m *MeterBandStats) ReadFrom(r io.Reader) (int64, error) {
+	return encoding.ReadFrom(r, &m.PacketBandCount, &m.ByteBandCount)
+}
+
+// meterStatsLen is a length of the meter stats message
+// without the list of meter band statistic.
+const meterStatsLen = 40
+
 type MeterStats struct {
-	MeterID       Meter
-	Length        uint16
-	_             pad6
+	Meter         Meter
 	FlowCount     uint32
 	PacketInCount uint64
 	ByteInCount   uint64
@@ -81,23 +279,36 @@ type MeterStats struct {
 	BandStats     []MeterBandStats
 }
 
-type MeterBandStats struct {
-	PacketBandCount uint64
-	ByteBandCount   uint64
+func (m *MeterStats) WriteTo(w io.Writer) (int64, error) {
+	var buf bytes.Buffer
+
+	// Write the list of meter band statistics into
+	// the termporary buffer to calculate the total
+	// message length.
+	_, err := encoding.WriteSliceTo(&buf, m.BandStats)
+	if err != nil {
+		return 0, err
+	}
+
+	length := uint16(meterStatsLen + buf.Len())
+
+	return encoding.WriteTo(w, m.Meter, length, pad6{},
+		m.FlowCount, m.PacketInCount, m.ByteInCount,
+		m.DurationSec, m.DurationNSec, buf.Bytes())
 }
 
-type MeterConfig struct {
-	Length  uint16
-	Flags   MeterModCommands
-	MeterID Meter
-	Bands   []MeterBandHeader
-}
+func (m *MeterStats) ReadFrom(r io.Reader) (int64, error) {
+	var length uint16
 
-type MeterFeatures struct {
-	MaxMeter     uint32
-	BandTypes    MeterBandType
-	Capabilities MeterFlags
-	MaxBands     uint8
-	MaxColor     uint8
-	_            pad2
+	// Unmarshal all elements from the reader except
+	// of the list of meter band statistics.
+	n, err := encoding.ReadFrom(r, &m.Meter, &length,
+		&defaultPad6, &m.FlowCount, &m.PacketInCount,
+		&m.ByteInCount, &m.DurationSec, &m.DurationNSec)
+
+	limrd := io.LimitReader(r, int64(length-meterStatsLen))
+	statsMaker := encoding.ReaderMakerOf(MeterBandStats{})
+
+	nn, err := encoding.ReadSliceFrom(limrd, statsMaker, m.BandStats)
+	return n + nn, err
 }
---
+++ ofp.v13/meter_test.go
@@ -0,0 +1,155 @@
+package ofp
+
+import (
+	"encoding/gob"
+	"testing"
+
+	"github.com/netrack/openflow/encoding/encodingtest"
+)
+
+func TestMeterMod(t *testing.T) {
+	bands := MeterBands{
+		&MeterBandDrop{64, 128},
+		&MeterBandDSCPRemark{128, 256, 6},
+		&MeterBandExperimenter{256, 512, 42},
+	}
+
+	tests := []encodingtest.MU{
+		{&MeterMod{
+			Command: MeterCommandModify,
+			Flags:   MeterFlagStats | MeterFlagBurst,
+			Meter:   Meter(42),
+			Bands:   bands,
+		}, []byte{
+			0x00, 0x01, // Meter command.
+			0x00, 0x0c, // Flags.
+			0x00, 0x00, 0x00, 0x2a, // Meter identifier.
+
+			// Band drop.
+			0x00, 0x01, // Meter type.
+			0x00, 0x10, // Meter length.
+			0x00, 0x00, 0x00, 0x40, // Rate.
+			0x00, 0x00, 0x00, 0x80, // Burst size.
+			0x00, 0x00, 0x00, 0x00, // 4-byte padding.
+
+			// Band DSCP remark.
+			0x00, 0x02, // Meter type.
+			0x00, 0x10, // Meter length.
+			0x00, 0x00, 0x00, 0x80, // Rate.
+			0x00, 0x00, 0x01, 0x00, // Burst size.
+			0x06,             // Precedence level.
+			0x00, 0x00, 0x00, // 3-byte padding.
+
+			// Band experimenter.
+			0xff, 0xff, // Meter type.
+			0x00, 0x10, // Meter length.
+			0x00, 0x00, 0x01, 0x00, // Rate.
+			0x00, 0x00, 0x02, 0x00, // Burst size.
+			0x00, 0x00, 0x00, 0x2a, // Experimenter.
+		}},
+	}
+
+	gob.Register(MeterBandDrop{})
+	gob.Register(MeterBandDSCPRemark{})
+	gob.Register(MeterBandExperimenter{})
+
+	encodingtest.RunMU(t, tests)
+}
+
+func TestMeterConfigRequest(t *testing.T) {
+	tests := []encodingtest.MU{
+		{&MeterConfigRequest{Meter(2)}, []byte{
+			0x00, 0x00, 0x00, 0x02, // Meter idenfitier.
+			0x00, 0x00, 0x00, 0x00, // 4-byte padding.
+		}},
+	}
+
+	encodingtest.RunMU(t, tests)
+}
+
+func TestMeterConfig(t *testing.T) {
+	tests := []encodingtest.MU{
+		{&MeterConfig{
+			Flags: MeterFlagKBitPerSec | MeterFlagBurst,
+			Meter: Meter(42),
+			Bands: MeterBands{&MeterBandDrop{64, 128}},
+		}, []byte{
+			0x00, 0x18, // Length.
+			0x00, 0x05, // Flags.
+			0x00, 0x00, 0x00, 0x2a, // Meter identifier.
+
+			// Band drop.
+			0x00, 0x01, // Meter type.
+			0x00, 0x10, // Meter length.
+			0x00, 0x00, 0x00, 0x40, // Rate.
+			0x00, 0x00, 0x00, 0x80, // Burst size.
+			0x00, 0x00, 0x00, 0x00, // 4-byte padding.
+		}},
+	}
+
+	encodingtest.RunMU(t, tests)
+}
+
+func TestMeterFeatures(t *testing.T) {
+	types := []MeterBandType{
+		MeterBandTypeDrop,
+		MeterBandTypeDSCPRemark,
+	}
+
+	tests := []encodingtest.MU{
+		{&MeterFeatures{
+			MaxMeter:     45,
+			BandTypes:    types,
+			Capabilities: MeterFlagBurst,
+			MaxBands:     128,
+			MaxColor:     16,
+		}, []byte{
+			0x00, 0x00, 0x00, 0x2d, // Max meter.
+			0x00, 0x01, // Band types.
+			0x00, 0x02,
+			0x00, 0x04, // Capabilities.
+			0x80,       // Max bands.
+			0x10,       // Max color.
+			0x00, 0x00, // 2-byte padding.
+		}},
+	}
+
+	encodingtest.RunMU(t, tests)
+}
+
+func TestMeterStats(t *testing.T) {
+	stats := []MeterBandStats{
+		{1413263059007179439, 7830709349700751879},
+		{15621460444570393343, 13154395619072477107},
+	}
+
+	tests := []encodingtest.MU{
+		{&MeterStats{
+			Meter:         Meter(42),
+			FlowCount:     2716600054,
+			PacketInCount: 3600438613393559849,
+			ByteInCount:   7110296996057607002,
+			DurationSec:   50,
+			DurationNSec:  10,
+			BandStats:     stats,
+		}, []byte{
+			0x00, 0x00, 0x00, 0x2a, // Meter identifier.
+			0x00, 0x48, // Length.
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // 6-byte padding.
+			0xa1, 0xec, 0x06, 0xf6, // Flow count.
+			0x31, 0xf7, 0x53, 0xd7, 0xca, 0xec, 0x51, 0x29, // Packet-in count.
+			0x62, 0xac, 0xd9, 0x72, 0x29, 0x8a, 0x6b, 0x5a, // Byte-in count.
+			0x00, 0x00, 0x00, 0x32, // Duration seconds.
+			0x00, 0x00, 0x00, 0x0a, // Duration nanoseconds.
+
+			// Band statistics.
+			0x13, 0x9c, 0xeb, 0x43, 0xae, 0x4e, 0x0a, 0xaf, // Packet count.
+			0x6c, 0xac, 0x44, 0xaa, 0x28, 0x3e, 0x52, 0x07, // Byte count.
+
+			0xd8, 0xca, 0x93, 0x82, 0x1f, 0x6f, 0x1a, 0xff, // Packet count.
+			0xb6, 0x8d, 0xcd, 0x1e, 0xdd, 0xc5, 0x07, 0xb3, // Byte count.
+		}},
+	}
+
+	encodingtest.RunMU(t, tests)
+}
