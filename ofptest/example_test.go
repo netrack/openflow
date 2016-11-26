d8c4ad64effedf962ff488a9906f7c2735cc64dd
Yasha Bubnov <ybubnou@infoblox.com>
2016-11-26 11:05:32 -0800

    Testing server documentation and example (#53)
    This patch contains the updated documentation for testing server
    and launchable example test.

--- ofptest/example_test.go
+++ ofptest/example_test.go
@@ -2,8 +2,10 @@ package ofptest_test
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/netrack/openflow"
+	"github.com/netrack/openflow/ofp"
 	"github.com/netrack/openflow/ofptest"
 )
 
@@ -18,4 +20,31 @@ func ExampleResponseRecorder() {
 	handler(w, req)
 	fmt.Printf("type: %d", w.First().Header.Type)
 	// Output: type: 0
+}
+
+func ExampleServer() {
+	ts := ofptest.NewServer(of.HandlerFunc(func(w of.ResponseWriter, r *of.Request) {
+		res := &ofp.EchoReply{Data: []byte("pong")}
+		w.Write(&of.Header{Type: of.TypeEchoReply}, res)
+	}))
+
+	defer ts.Close()
+
+	echoReq := &ofp.EchoRequest{Data: []byte("ping")}
+	req := of.NewRequest(of.TypeEchoRequest, echoReq)
+
+	conn, err := of.Dial("tcp", ts.Listener.Addr().String())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn.Send(req)
+	conn.Flush()
+	resp, _ := conn.Receive()
+
+	var echoResp ofp.EchoReply
+	echoResp.ReadFrom(resp.Body)
+
+	fmt.Printf("%s", echoResp.Data)
+	// Output: pong
 }
