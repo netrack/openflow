ce75156b2b039dfe8ea38bb18ac9697fb12cf190
Yasha Bubnov <girokompass@gmail.com>
2016-11-26 22:05:00 +0300

    Testing server documentation and example
    This patch contains the updated documentation for testing server
    and launchable example test.

--- ofptest/server.go
+++ ofptest/server.go
@@ -10,9 +10,15 @@ import (
 	"github.com/netrack/openflow"
 )
 
+// Server is an OpenFlow server listening on a system-chosen port
+// on the loopback interface, for use in end-to-end OpenFlow tests.
 type Server struct {
+	// Listener specifies the server listener. This attribute is
+	// optional, if specified, will be used instead of the default.
 	Listener net.Listener
 
+	// Config may be changed after calling NewUnstartedServer and
+	// before Start.
 	Config *of.Server
 
 	closed bool
@@ -21,12 +27,16 @@ type Server struct {
 	mu sync.Mutex
 }
 
+// NewServer starts and returns a new Server. The caller should call
+// Close when finished to shut it down.
 func NewServer(handler of.Handler) *Server {
 	srv := NewUnstartedServer(handler, nil)
 	srv.Start()
 	return srv
 }
 
+// newLocalListener creates a new listener on loopback interface with
+// a system-chosen port.
 func newLocalListener() net.Listener {
 	urls := []*url.URL{
 		{Scheme: "tcp", Host: "127.0.0.1:0"},
@@ -48,6 +58,11 @@ func newLocalListener() net.Listener {
 	return nil
 }
 
+// NewUnstartedServer returns a new Server but doesn't start it.
+//
+// After changing its configuration, the caller should call Start.
+//
+// The caller should call Close when finished to shut it down.
 func NewUnstartedServer(handler of.Handler, listener net.Listener) *Server {
 	if listener == nil {
 		listener = newLocalListener()
@@ -59,6 +74,8 @@ func NewUnstartedServer(handler of.Handler, listener net.Listener) *Server {
 	}
 }
 
+// wrap wraps the server ConnState function in order to save all
+// incoming connections to clean them up at the server shut down.
 func (s *Server) wrap() {
 	oldCb := s.Config.ConnState
 
@@ -82,6 +99,7 @@ func (s *Server) wrap() {
 
 }
 
+// Start starts a server on system-chosen port.
 func (s *Server) Start() {
 	s.wrap()
 
@@ -90,17 +108,20 @@ func (s *Server) Start() {
 	}()
 }
 
+// closeChanConn closes a specified connection an sends a message
+// to the cannel to notify the caller about the termination.
 func (s *Server) closeChanConn(conn of.Conn, ch chan struct{}) {
 	conn.Close()
 	ch <- struct{}{}
 }
 
+// CloseClientConnections closes all open connections to the Server.
 func (s *Server) CloseClientConnections() {
 	nconns := len(s.conns)
 	ch := make(chan struct{}, nconns)
 
 	for conn := range s.conns {
-		s.closeChanConn(conn, ch)
+		go s.closeChanConn(conn, ch)
 	}
 
 	timer := time.NewTimer(5 * time.Second)
@@ -115,6 +136,8 @@ func (s *Server) CloseClientConnections() {
 	}
 }
 
+// Close shuts down the server and closes all open connections to the
+// Server.
 func (s *Server) Close() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
